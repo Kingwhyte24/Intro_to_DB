3a26d9b47d8c228013cc954697565e238e325d7e
Kingwhyte24 <ohagbanusikingsley@gmail.com>
2025-02-24 15:35:56 +0100

    MySQLServer

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,19 +1,20 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="password"
-)
+try:
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password"
+    )
 
-if db.is_connected():
-    mycursor = db.cursor()
+    if db.is_connected():
+        mycursor = db.cursor()
 
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-    print("Database 'alx_book_store' created successfully!")
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
 
-    mycursor.close()
-    db.close()
+        mycursor.close()
+        db.close()
 
-else:
-    print("Connection Failed!")
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
